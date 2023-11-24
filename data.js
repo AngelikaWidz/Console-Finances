d97e58e70b2171a870230e2293d388cb412d8d63
AngelikaWidz <111324775+AngelikaWidz@users.noreply.github.com>
2023-11-24 00:51:58 +0000

    Added Initial Pseudocode before beginning task

---
+++ data.js
@@ -0,0 +1,26 @@
+// Import data/code to be able to begin comparing data
+// using a link?
+
+
+// Way of accessing arrays using the code
+
+
+// Access dates to find out how many dates there are in total - create var = totalMonths
+// Using a function which adds together?
+// Then print the amount of months to console/console.log()
+
+
+// Acess Profit/Losses array - create var = PL (Profits/Losses access)
+// Add together all Profits/Losses - create var totalPL
+// Use totalMonths from previous to do totalPL/totalMonths = averageChange
+// Print averageChange to console/console.log()
+
+
+// Greatest profits 
+// mostPL = [0]
+// if PL > mostPL: Print/console.log()
+
+
+// Least Profits
+// leastPL = [0]
+// if PL < leastPL: Print/console.log()
